9312553444a7a973fd77bd2bb7a0cd08f7b26589
mukesh <mk96952642@gmail.com>
2023-04-29 13:51:04 +0530

    calculator program without exception handling

---
+++ w3.py
@@ -0,0 +1,17 @@
+sum= int(input("enter the vegetable:"))
+sum1 = int(input("tomato:"))
+sum2 = int(input("patato:"))
+sum3 = int(input("flower:"))
+sum4 = int(input("cabbage:"))
+sum5 = int(input("peas:"))
+sum6 = int(input("okra:"))
+sum7 = int(input("spinach:"))
+sum8 = int(input("chill:"))
+sum9 = int(input("lemon:"))
+sum10 = int(input("coriander:"))
+sum11 = int(input("capsicum:"))
+sum12 = int(input("ginger:"))
+
+total = sum1 + sum2 + sum3 + sum3 + sum4 + sum5 + sum5 + sum6 + sum7 + sum8 + sum9 + sum10 + sum11
+
+print(("total vegetable:", total))
